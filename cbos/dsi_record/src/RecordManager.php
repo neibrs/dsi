acbb8d03c68d41acc44e8761acaee93e735212ea
John.nie <John-lee@2001594.no-reply.drupal.org>
2020-11-17 15:05:19 +0800

    Add record calendar.

---
+++ cbos/dsi_record/src/RecordManager.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\dsi_record;
+
+use Drupal\layout_builder\Section;
+use Drupal\layout_builder\SectionComponent;
+
+class RecordManager implements RecordManagerInterface {
+
+  public function getRecordSection() {
+    $section = new Section('layout_twocol_section', [
+      'column_widths' => '25-75',
+    ], [
+      'views_calendar' => new SectionComponent('views_calender', 'second', [
+        'id' => 'record_calender',
+        'label' => 'Record calender',
+        'label_display' => FALSE,
+      ]),
+    ]);
+
+    return $section;
+  }
+}
