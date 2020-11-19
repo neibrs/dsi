a591b1b13db2bfb7ba92144c75e1c5a029fdcd37
John.nie <John-lee@2001594.no-reply.drupal.org>
2020-11-19 10:23:00 +0800

    Add field plugin for entity_id.

--- industry/dsi_client/src/Entity/ClientViewsData.php
+++ industry/dsi_client/src/Entity/ClientViewsData.php
@@ -17,6 +17,7 @@ class ClientViewsData extends EntityViewsData {
 
     // Additional information for Views integration, such as table joins, can be
     // put here.
+    $data['dsi_client_field_data']['entity_id']['field']['id'] = 'client_sign';
     return $data;
   }
 
