657caa88fd66a3573b9c3ffdb70cf6f7959ba003
John.nie <John-lee@2001594.no-reply.drupal.org>
2020-11-24 15:45:58 +0800

    Add blocks module for color theme.

---
+++ themes/dsi_color/modules/dsi_color_block/src/Plugin/Block/NavbarUserBlock.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\dsi_color_block\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'NavbarUserBlock' block.
+ *
+ * @Block(
+ *  id = "navbar_user_block",
+ *  admin_label = @Translation("Navbar user block"),
+ * )
+ */
+class NavbarUserBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+    $build['#theme'] = 'navbar_user_block';
+     $build['navbar_user_block']['#markup'] = 'Implement NavbarUserBlock.';
+
+    return $build;
+  }
+
+}
