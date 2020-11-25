77c55ef0d5a1408b313c511bf695e11b7397c3fb
John.nie <80285394@qq.com>
2020-11-25 17:45:35 +0800

    Fix modal z-index.

--- themes/dsi_color/js/base.js
+++ themes/dsi_color/js/base.js
@@ -1,6 +1,9 @@
 (function ($, Drupal, drupalSettings) {
   Drupal.behaviors.color_base = {
     attach: function attach(context, settings) {
+      $(document).once('modal-z-index').ready(function () {
+        $('#drupal-modal').parent().addClass('z-index-1030');
+      });
     }
   };
 })(jQuery, Drupal, drupalSettings);
