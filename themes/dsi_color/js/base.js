555cf93eaa31a5f6c6da1154f0ef5ac2892e567b
John.nie <80285394@qq.com>
2020-11-25 21:08:58 +0800

    Greater than claro theme.css.

--- themes/dsi_color/js/base.js
+++ themes/dsi_color/js/base.js
@@ -2,7 +2,7 @@
   Drupal.behaviors.color_base = {
     attach: function attach(context, settings) {
       $(document).once('modal-z-index').ready(function () {
-        $('#drupal-modal').parent().addClass('z-index-1030');
+        $('#drupal-modal').parent().addClass('z-index-1360');
       });
     }
   };
