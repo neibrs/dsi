c927685ac13658c2d534259ce236fcd9103f2736
John.nie <80285394@qq.com>
2020-11-27 14:23:42 +0800

    Add detail records.

--- themes/dsi_color/js/base.js
+++ themes/dsi_color/js/base.js
@@ -4,6 +4,29 @@
       $(document).once('modal-z-index').ready(function () {
         $('#drupal-modal').parent().addClass('z-index-1360');
       });
+
+      $('.record-ajax-tr').once('record-ajax-tr').on('click', function() {
+        let $tr = $(this).parent();
+        let $entity_type = $tr.attr('entity-type');
+        let $entity_id = $tr.attr('entity-id');
+        let $td_count = $tr.children().length;
+
+        let url = Drupal.url('ajax/dsi_record/'+ $entity_type + '/' + $entity_id + '/list');
+        let $search_key = $(context).find('tr[entity-id-child="$entity_id"]');
+        if ($search_key.length) {
+          $search_key.remove();
+        }
+        else {
+          $.get({
+            url: url,
+            success: function(data){
+              let content = '<tr entity-type='+ $entity_type + ' entity-id-child='+ $entity_id + '><td colspan='+ $td_count + '>' + data + '</td></tr>';
+              $tr.after(content);
+            }
+          });
+        }
+
+      });
     }
   };
 })(jQuery, Drupal, drupalSettings);
