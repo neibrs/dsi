ac84b51112f1bc8063199775c29517e361e97671
John.nie <John-lee@2001594.no-reply.drupal.org>
2020-11-28 10:46:30 +0800

    Polish.

--- cbos/dsi_record/src/Controller/RecordController.php
+++ cbos/dsi_record/src/Controller/RecordController.php
@@ -3,6 +3,8 @@
 namespace Drupal\dsi_record\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\layout_builder\Section;
+use Drupal\layout_builder\SectionComponent;
 
 /**
  * Returns responses for record routes.
@@ -24,12 +26,21 @@ class RecordController extends ControllerBase {
 
   public function getRecordsByEntity($entity_type, $entity_id) {
 
-    $build['entities'] = \Drupal::service('plugin.manager.block')->createInstance('record_by_entity_block', [
-      'entity_type' => $entity_type,
-      'entity_id' => $entity_id
-    ])->build();
+//    $build['entities'] = \Drupal::service('plugin.manager.block')->createInstance('record_by_entity_block', [
+//      'entity_type' => $entity_type,
+//      'entity_id' => $entity_id
+//    ])->build();
+    $section = new Section('layout_twocol_section', [
+      'column_widths' => '25-75',
+    ], [
+      'record' => new SectionComponent('record', 'first', [
+        'id' => 'record_by_entity_block',
+        'label' => 'Record entity block',
+        'label_display' => FALSE,
+      ]),
+    ]);
 
-
+    $build = $section->toRenderArray();
     return $build;
   }
 
