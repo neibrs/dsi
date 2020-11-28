bc44c47a591c8e13213af0ca3ee10a864bed6b01
John.nie <80285394@qq.com>
2020-11-28 12:24:17 +0800

    Polish client intro block.

--- industry/dsi_client/src/Plugin/Block/ClientIntroBlock.php
+++ industry/dsi_client/src/Plugin/Block/ClientIntroBlock.php
@@ -14,16 +14,28 @@ use Drupal\Core\Block\BlockBase;
  */
 class ClientIntroBlock extends BlockBase {
 
+  public function defaultConfiguration() {
+    return [
+      'entity_type' => '',
+      'entity_id' => '',
+    ];
+  }
+
   /**
    * {@inheritdoc}
    */
   public function build() {
     $build = [];
     $build['#theme'] = 'client_intro_block';
-    $build['#content']['#markup'] = 'Implement ClientIntroBlock.';
 
+    if (empty($this->configuration['entity_type']) || empty($this->configuration['entity_id'])) {
+      return $build;
+    }
     // add link
     // list
+    $client = \Drupal::entityTypeManager()->getStorage($this->configuration['entity_type'])->load($this->configuration['entity_id']);
+    $build['#content']['add_link'] = $client->toLink('简单修改', 'edit-form', ['dsi_client' => $this->configuration['entity_id']])->toRenderable();
+    $build['#content']['intro'] = $client->get('summary')->value;
 
     return $build;
   }
