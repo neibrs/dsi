7737dd8341dc6c72a1bab56cca1247835aeabff1
John.nie <80285394@qq.com>
2020-12-05 14:35:48 +0800

    Add data.

--- industry/dsi_client/src/Plugin/migrate/destination/Client.php
+++ industry/dsi_client/src/Plugin/migrate/destination/Client.php
@@ -19,10 +19,9 @@ class Client extends EntityContentBase {
   public function import(Row $row, array $old_destination_id_values = []) {
     // TODO, 当前client需要从回访记录里面获取最近的时间，设置到client修改时间作为客户的最近跟进时间
     $ids = parent::import($row, $old_destination_id_values);
-    
 
-    $queue = \Drupal::queue('client_record_process');
-    $queue->createItem(['ids' => $ids, 'row' => $row]);
+//    $queue = \Drupal::queue('client_record_process');
+//    $queue->createItem(['ids' => $ids, 'row' => $row]);
     return $ids;
   }
   
