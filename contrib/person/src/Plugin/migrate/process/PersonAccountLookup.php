52b676b93dd2567a49cedfbb435fa152fcdca8e9
John.nie <80285394@qq.com>
2020-12-06 11:14:55 +0800

    Refactor.

--- contrib/person/src/Plugin/migrate/process/PersonAccountLookup.php
+++ contrib/person/src/Plugin/migrate/process/PersonAccountLookup.php
@@ -15,7 +15,7 @@ class PersonAccountLookup extends ProcessPluginBase {
   
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
     if (empty($value)) {
-      return NULL;
+      return 0;
     }
     $user = \Drupal::entityTypeManager()->getStorage('user')->loadByProperties([
       'person' => $value,
