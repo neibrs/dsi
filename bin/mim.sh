791280dc89472cdfc32d125c5b8b31ff6b763408
John.nie <80285394@qq.com>
2020-12-06 23:41:52 +0800

    fix.

--- bin/mim.sh
+++ bin/mim.sh
@@ -4,7 +4,7 @@ DRUPAL="$(pwd)"
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 # Enable lawyer industry
-vendor/bin/drush mim contract_xlsx
+#vendor/bin/drush mim contract_xlsx
 vendor/bin/drush mim 30_client_xlsx
 vendor/bin/drush mim 30_record_xlsx
 vendor/bin/drush mim 208_client_xlsx
