5afa7d00ddf19feed4a49e9b3f3fae63d580c01b
John <80285394@qq.com>
2020-12-27 13:44:55 +0800

    Fixed, translation in new.

--- bin/translation.sh
+++ bin/translation.sh
@@ -3,13 +3,15 @@
 vendor/bin/drush en potx
 echo $(pwd)
 
-for file in `find modules/dsi -name "*.info.yml"`; do
+for file in `find web/modules/dsi -name "*.info.yml"`; do
   echo $(dirname $file) -- $(basename $(dirname $file));
-  $(pwd)/vendor/bin/drush potx single --include=modules/contrib/potx --folder="$(dirname $file)/" --api=9
+  path=$(dirname $file);
+  echo ${path#*web/}
+  $(pwd)/vendor/bin/drush potx single --include=web/modules/contrib/potx/ --folder="${path#*web/}/" --api=9
   if [ ! -d $(dirname $file)/translations ]; then
     mkdir $(dirname $file)/translations;
     echo "mkdir $(dirname $file)/translations";
   fi
-  mv "$(pwd)/general.pot" "$(dirname $file)/translations/$(basename $(dirname $file)).pot"
+  mv "$(pwd)/web/general.pot" "$(dirname $file)/translations/$(basename $(dirname $file)).pot"
   echo 'DONE'
 done
