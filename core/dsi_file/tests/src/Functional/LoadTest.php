a6623bc091172ad7903a3fe2680e5b17938ee0c3
John <80285394@qq.com>
2020-12-28 14:10:57 +0800

    Refactor the file module.

--- core/dsi_file/tests/src/Functional/LoadTest.php
+++ core/dsi_file/tests/src/Functional/LoadTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Drupal\Tests\dsi_files\Functional;
+namespace Drupal\Tests\dsi_file\Functional;
 
 use Drupal\Core\Url;
 use Drupal\Tests\BrowserTestBase;
@@ -8,7 +8,7 @@ use Drupal\Tests\BrowserTestBase;
 /**
  * Simple test to ensure that main page loads with module enabled.
  *
- * @group dsi_files
+ * @group dsi_file
  */
 class LoadTest extends BrowserTestBase {
 
@@ -17,7 +17,7 @@ class LoadTest extends BrowserTestBase {
    *
    * @var array
    */
-  public static $modules = ['dsi_files'];
+  public static $modules = ['dsi_file'];
 
   /**
    * A user with permission to administer site configuration.
