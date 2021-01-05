f586130568103cff6bf96d5c8c8f030f97aa59b7
John <80285394@qq.com>
2021-01-05 10:02:50 +0800

    Fix.

--- cbos/dsi_attachment/src/Controller/AttachmentController.php
+++ cbos/dsi_attachment/src/Controller/AttachmentController.php
@@ -6,6 +6,9 @@ use Drupal\Core\Controller\ControllerBase;
 
 class AttachmentController extends ControllerBase {
 
+  /**
+   * Build document list.
+   */
   public function getAttachments() {
     $build['#theme'] = 'attachment_entity';
     $build['#attached']['library'][] = 'dsi_attachment/attachment';
